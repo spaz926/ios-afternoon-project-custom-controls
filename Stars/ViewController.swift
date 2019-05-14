ad743f1ab8a139b2b9bec02f00e9839cbe87715d
Jeffrey Carpenter <spaz926@me.com>
2019-05-14 12:29:27 -0500

    Create initial Xcode project

---
+++ Stars/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stars
+//
+//  Created by Jeffrey Carpenter on 5/14/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
