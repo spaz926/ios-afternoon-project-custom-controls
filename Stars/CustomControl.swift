f9a6933f4b83d9caa71a0594f8f852ef5083bd1a
Jeffrey Carpenter <spaz926@me.com>
2019-05-14 13:42:30 -0500

    Add custom control

---
+++ Stars/CustomControl.swift
@@ -0,0 +1,22 @@
+//
+//  CustomControl.swift
+//  Stars
+//
+//  Created by Jeffrey Carpenter on 5/14/19.
+//  Copyright © 2019 Jeffrey Carpenter. All rights reserved.
+//
+
+import UIKit
+
+class CustomControl: UIControl {
+    
+    var value = 1
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
